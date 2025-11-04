4712266dd7f8b88d9d6f7c022eed1a2ce11b2d0f
wanseau <yoohaang01@gmail.com>
2025-11-04 11:03:17 +0800

    Added trusted origins

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://peitelbackend.onrender.com" 
+]
